a64734564dbb7bbaa00c81292b2355f191ca1727
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-22 22:10:04 -0300

    entidades criadas

---
+++ nlw5/src/entities/Settings.ts
@@ -0,0 +1,35 @@
+import { 
+    Entity,
+    PrimaryColumn, 
+    Column, 
+    UpdateDateColumn,
+    CreateDateColumn 
+} from "typeorm";
+
+import { v4 as uuid } from "uuid";
+
+@Entity("settings")
+class Setting {
+    @PrimaryColumn()
+    id: string;
+
+    @Column()
+    username: string;
+    
+    @Column()
+    chat: boolean;
+    
+    @UpdateDateColumn()
+    updated_at: Date;
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    constructor() {
+        if(!this.id) {
+            this.id = uuid();
+        }
+    }
+}
+
+export { Setting }
