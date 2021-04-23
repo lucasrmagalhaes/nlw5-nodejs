52ff2fde5618053afb5bb117395f4793558875a7
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-22 22:41:19 -0300

    controller criado. aula 2 finalizada!

--- nlw5/src/routes.ts
+++ nlw5/src/routes.ts
@@ -1,32 +1,10 @@
 import { Router } from "express";
-import { getCustomRepository } from "typeorm";
-import { SettingsRepository } from "./repositories/SettingsRepository";
+import { SettingsController } from "./controllers/SettingsController";
 
 const routes = Router();
 
-/**
- * Tipos de parametros
- * 
- * Routes Params => Parametros de rotas
- * Query Params => Filtros e buscas
- * Body Params => {
- * 
- * }
- */
+const settingsController = new SettingsController();
 
-routes.post("/settings", async (request, response) => {
-    const { chat, username } = request.body;
-
-    const settingsRepository = getCustomRepository(SettingsRepository);
-
-    const settings = settingsRepository.create({
-        chat,
-        username
-    })
-
-    await settingsRepository.save(settings);
-
-    return response.json(settings);
-});
+routes.post("/settings", settingsController.create);
 
 export { routes };
