4f35d8460044d8bc0bbbb4fe8abe279577e5b941
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-24 19:55:29 -0300

    front-end e iniciando o websocket

--- nlw5/src/server.ts
+++ nlw5/src/server.ts
@@ -1,10 +1,4 @@
-import express from "express";
-import "./database";
-import { routes } from "./routes";
+import { http } from "./http";
+import "./websocket/client";
 
-const app = express();
-
-app.use(express.json());
-
-app.use(routes);
-app.listen(3333, () => console.log("Server is running on port 3333."));
+http.listen(3333, () => console.log("Server is running on port 3333."));
