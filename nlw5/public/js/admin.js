784243e1ad5d40794a739a52db0f1de29cf20455
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-25 15:06:20 -0300

    pegando o atendimento

--- nlw5/public/js/admin.js
+++ nlw5/public/js/admin.js
@@ -1,6 +1,8 @@
 const socket = io();
+let connectionsUsers = [];
 
 socket.on("admin_list_all_users", (connections) => {
+    connectionsUsers = connections;
     document.getElementById("list_users").innerHTML = "";
 
     let template = document.getElementById("template").innerHTML;
@@ -14,3 +16,17 @@ socket.on("admin_list_all_users", (connections) => {
         document.getElementById("list_users").innerHTML += rendered;
     });
 });
+
+function call(id) {
+    // Percorrendo o array
+    const connection = connectionsUsers.find(connection => connection.socket_id === id);
+
+    const template = document.getElementById("admin_template").innerHTML;
+
+    const rendered = Mustache.render(template, {
+        email: connection.user.email,
+        id: connection.user_id
+    });
+
+    document.getElementById("supports").innerHTML += rendered;
+}
