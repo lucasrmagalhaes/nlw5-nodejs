cb21bf3dc967e52c58c66b25d3792c9a731b1dae
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-25 14:39:38 -0300

    listando mensagens do banco

--- nlw5/public/js/chat.js
+++ nlw5/public/js/chat.js
@@ -20,10 +20,26 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
         };
 
         socket.emit("client_first_access", params, (call, err) => {
-            if(err) {
+            if (err) {
                 console.err(err);
             } else {
                 console.log(call);
+            }
+        });
+    });
+
+    socket.on("client_list_all_messages", (messages) => {
+        var template_client = document.getElementById("message-user-template").innerHTML;
+        var template_admin = document.getElementById("admin-template").innerHTML;
+
+        messages.forEach((message) => {
+            if (message.admin_id === null) {
+                const rendered = Mustache.render(template_client, {
+                    message: message.text,
+                    email
+                });
+
+                document.getElementById("messages").innerHTML += rendered
             }
         });
     });
--- nlw5/src/websocket/client.ts
+++ nlw5/src/websocket/client.ts
@@ -49,5 +49,9 @@ io.on("connect", (socket) => {
             text,
             user_id
         })
+
+        const allMessages = await messagesService.listByUser(user_id);
+
+        socket.emit("client_list_all_messages", allMessages);
     });
 });
