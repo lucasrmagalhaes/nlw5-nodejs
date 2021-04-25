aa86e3ddaf7da40dac0954aba0bcc2f18cedcde3
Lucas Magalhães <lucasdarosa.ti@gmail.com>
2021-04-25 20:17:15 -0300

    cliente recebendo mensagens

--- nlw5/public/js/admin.js
+++ nlw5/public/js/admin.js
@@ -73,5 +73,18 @@ function sendMessage(id) {
     }
 
     socket.emit("admin_send_message", params);
+
+    const divMessages = document.getElementById(`allMessages${id}`);
+    
+    const createDiv = document.createElement("div");
+    createDiv.className = "admin_message_admin";
+
+    createDiv.innerHTML = `Atendente: <span>${params.text}</span>`;
+    createDiv.innerHTML += `<span class="admin_date">${dayjs()
+        .format("DD/MM/YYYY HH:mm:ss")}</span>`;
+
+    divMessages.appendChild(createDiv);
+
+    text.value = "";
 }
 
--- nlw5/public/js/chat.js
+++ nlw5/public/js/chat.js
@@ -1,3 +1,5 @@
+let socket_admin_id = null;
+
 document.querySelector("#start_chat").addEventListener("click", (event) => {
     const socket = io();
 
@@ -48,7 +50,15 @@ document.querySelector("#start_chat").addEventListener("click", (event) => {
         });
     });
 
-    socket.on("admin_send_to_client", message => {
-        console.log(message);
+    socket.on("admin_send_to_client", (message) => {
+        socket_admin_id = message.socket_id;
+
+        const template_admin = document.getElementById("admin-template").innerHTML;
+
+        const rendered = Mustache.render(template_admin, {
+            message_admin: message.text
+        });
+
+        document.getElementById("messages").innerHTML += rendered;
     });
 });
